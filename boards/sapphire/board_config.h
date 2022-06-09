25b7ec2e265d80862defb960103e791df6647fc1
Chris Hayward <hayward@smu.edu>
2022-06-09 11:29:48 -0500

    Added sapphire to board support

---
+++ boards/sapphire/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define	EXTERNAL_OSC
+#define VENDOR_NAME "GeoAcoustics Research"
+#define PRODUCT_NAME "Sapphire S6"
+#define VOLUME_LABEL "GACBOOT"
+#define INDEX_URL "http://www.smu.edu/"
+#define BOARD_ID "SAMD21G18A-Sapphire-s6"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA17
+
+#endif
---
+++ boards/sapphire_mini/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define	EXTERNAL_OSC 1
+#define VENDOR_NAME "GeoAcoustics Research"
+#define PRODUCT_NAME "Sapphire Mini"
+#define VOLUME_LABEL "GACBOOT"
+#define INDEX_URL "http://www.smu.edu/"
+#define BOARD_ID "SAMD21E18A-Sapphire-Mini"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA02
+
+#endif
